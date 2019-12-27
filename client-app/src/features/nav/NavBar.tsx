5a457dacbed602c48c2487e55ee93f8b135578eb
bozomustapic <bozo.mustapic@dekra.com>
2019-12-27 13:03:55 +0100

    Refactoring the create activity method

--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -1,25 +1,34 @@
-import React, {Fragment} from "react";
+import React, { Fragment, useContext } from "react";
 import { Menu, Container, Button } from "semantic-ui-react";
+import ActivityStore from "../../app/stores/activityStore";
+import { observer } from "mobx-react-lite";
 
-interface IProps {
-  openCreateForm: () => void;
-}
-
-export const NavBar: React.FC<IProps> = ({openCreateForm}) => {
+const NavBar: React.FC = () => {
+  const activityStore = useContext(ActivityStore);
   return (
     <Fragment>
       <Menu fixed="top" inverted>
         <Container>
           <Menu.Item header>
-            <img src="/assets/logo.png" alt="logo" style={{marginRight: '10px'}}/>
+            <img
+              src="/assets/logo.png"
+              alt="logo"
+              style={{ marginRight: "10px" }}
+            />
             Reactivities
           </Menu.Item>
           <Menu.Item name="Activities" />
           <Menu.Item>
-            <Button onClick={openCreateForm} positive content="Create Activity" />
+            <Button
+              onClick={activityStore.openCreateForm}
+              positive
+              content="Create Activity"
+            />
           </Menu.Item>
         </Container>
       </Menu>
     </Fragment>
   );
 };
+
+export default observer(NavBar);
