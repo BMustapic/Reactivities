1c7006de3e324d6214dc7137d7479b38863ab865
bozomustapic <bozo.mustapic@dekra.com>
2019-12-30 11:09:08 +0100

    Using Axios interceptors to catch errors

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,12 +3,18 @@ import { Container } from "semantic-ui-react";
 import NavBar from "../../features/nav/NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import { observer } from "mobx-react-lite";
-import { Route, withRouter, RouteComponentProps } from "react-router-dom";
+import {
+  Route,
+  withRouter,
+  RouteComponentProps,
+  Switch
+} from "react-router-dom";
 import HomePage from "../../features/home/HomePage";
 import ActivityForm from "../../features/activities/form/ActivityForm";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
+import NotFound from "../layout/NotFound";
 
-const App: React.FC<RouteComponentProps> = ({location}) => {
+const App: React.FC<RouteComponentProps> = ({ location }) => {
   return (
     <Fragment>
       <Route exact path="/" component={HomePage} />
@@ -18,13 +24,16 @@ const App: React.FC<RouteComponentProps> = ({location}) => {
           <Fragment>
             <NavBar />
             <Container style={{ marginTop: "7em" }}>
-              <Route path="/activities" exact component={ActivityDashboard} />
-              <Route path="/activities/:id" component={ActivityDetails} />
-              <Route
-                path={["/createActivity", "/manage/:id"]}
-                component={ActivityForm}
-                key={location.key}
-              />
+              <Switch>
+                <Route path="/activities" exact component={ActivityDashboard} />
+                <Route path="/activities/:id" component={ActivityDetails} />
+                <Route
+                  path={["/createActivity", "/manage/:id"]}
+                  component={ActivityForm}
+                  key={location.key}
+                />
+                <Route component={NotFound} />
+              </Switch>
             </Container>
           </Fragment>
         )}
