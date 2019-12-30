1fc5ddc1cce3648ffd0ba6afe4a51147090c8c1d
bozomustapic <bozo.mustapic@dekra.com>
2019-12-30 11:33:18 +0100

    Adding toast notifications

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Router } from "react-router-dom";
-import {createBrowserHistory} from 'history';
+import { createBrowserHistory } from "history";
+import "react-toastify/dist/ReactToastify.min.css";
 import "./app/layout/styles.css";
 import App from "./app/layout/App";
 import * as serviceWorker from "./serviceWorker";
-import ScrollToTop from './app/layout/ScrollToTop'
+import ScrollToTop from "./app/layout/ScrollToTop";
 
 export const history = createBrowserHistory();
 
 ReactDOM.render(
   <Router history={history}>
-    <ScrollToTop><App /></ScrollToTop>
+    <ScrollToTop>
+      <App />
+    </ScrollToTop>
   </Router>,
   document.getElementById("root")
 );
