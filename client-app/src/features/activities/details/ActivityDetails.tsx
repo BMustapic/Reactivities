b219d61a806f8fa483592293014e93fa19766bf4
bozomustapic <bozo.mustapic@dekra.com>
2020-01-07 15:13:34 +0100

    Creating a MobX root store

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -8,6 +8,7 @@ import ActivityDetailedHeader from "./ActivityDetailedHeader";
 import ActivityDetailedInfo from "./ActivityDetailedInfo";
 import ActivityDetailedChat from "./ActivityDetailedChat";
 import ActivityDetailedSidebar from "./ActivityDetailedSidebar";
+import { RootStoreContext } from "../../../app/stores/rootStore";
 
 interface IDetailParams {
   id: string;
@@ -15,8 +16,8 @@ interface IDetailParams {
 
 const ActivityDetails: React.FC<RouteComponentProps<IDetailParams>> = ({
   match}) => {
-  const activityStore = useContext(ActivityStore);
-  const { activity, loadActivity, loadingInitial } = activityStore;
+  const rootStore = useContext(RootStoreContext);
+  const { activity, loadActivity, loadingInitial } = rootStore.activityStore;
 
   useEffect(() => {
     loadActivity(match.params.id);
