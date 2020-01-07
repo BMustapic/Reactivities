3b9659b2b7065b18978e1cd7a3856cc5fb0c94d7
bozomustapic <bozo.mustapic@dekra.com>
2020-01-07 09:15:21 +0100

    Creating a reusable Text Area Input field

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -7,6 +7,7 @@ import { observer } from "mobx-react-lite";
 import { RouteComponentProps } from "react-router-dom";
 import { Form as FinalForm, Field } from "react-final-form";
 import TextInput from "../../../app/common/form/TextInput";
+import TextAreaInput from "../../../app/common/form/TextAreaInput";
 
 interface IDetailParams {
   id: string;
@@ -91,7 +92,8 @@ const ActivityForm: React.FC<RouteComponentProps<IDetailParams>> = ({
                   name="description"
                   placeholder="Description"
                   value={activity.description}
-                  component={TextInput}
+                  component={TextAreaInput}
+                  rows={3}
                 />
                 <Field
                   name="category"
