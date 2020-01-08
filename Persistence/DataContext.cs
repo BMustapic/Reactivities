e34c0f097295ae43cdec7fe4133c454529f9094e
bozomustapic <bozo.mustapic@dekra.com>
2020-01-08 10:07:18 +0100

    Adding a class to join our Users and Activities

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -13,6 +13,7 @@ namespace Persistence
 
         public DbSet<Value> Values { get; set; }
         public DbSet<Activity> Activities { get; set; }
+        public DbSet<UserActivity> UserActivities { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -20,22 +21,35 @@ namespace Persistence
 
             modelBuilder.Entity<Value>()
                 .HasData(
-                    new Value 
+                    new Value
                     {
                         ID = 1,
                         Name = "Value 101"
                     },
-                    new Value 
+                    new Value
                     {
                         ID = 2,
                         Name = "Value 102"
                     },
-                    new Value 
+                    new Value
                     {
                         ID = 3,
                         Name = "Value 103"
                     }
                 );
+
+            modelBuilder.Entity<UserActivity>(x => x.HasKey(ua =>
+                new { ua.AppUserId, ua.ActivityId }));
+
+            modelBuilder.Entity<UserActivity>()
+                .HasOne(u => u.AppUser)
+                .WithMany(a => a.UserActivities)
+                .HasForeignKey(u => u.AppUserId);
+
+            modelBuilder.Entity<UserActivity>()
+                .HasOne(a => a.Activity)
+                .WithMany(u => u.UserActivities)
+                .HasForeignKey(a => a.ActivityId);
         }
     }
 }
