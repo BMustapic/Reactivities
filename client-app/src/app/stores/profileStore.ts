4ef4a4119bb7a23896c46925ae4bd4165f484412
bozomustapic <bozo.mustapic@dekra.com>
2020-01-09 13:30:26 +0100

    Conditionally rendering the Photo Upload Widget

--- client-app/src/app/stores/profileStore.ts
+++ client-app/src/app/stores/profileStore.ts
@@ -1,5 +1,5 @@
 import { RootStore } from "./rootStore";
-import { observable, action, runInAction } from "mobx";
+import { observable, action, runInAction, computed } from "mobx";
 import { IProfile } from "../models/profile";
 import agent from "../api/agent";
 
@@ -11,6 +11,16 @@ export default class ProfileStore {
 
   @observable profile: IProfile | null = null;
   @observable loadingProfile = true;
+
+  @computed get isCurrentUser() {
+    if (this.rootStore.userStore.user && this.profile) {
+      console.log("store", this.rootStore.userStore.user.username);
+      console.log("profile", this.profile.username);
+      return this.rootStore.userStore.user.username === this.profile.username;
+    } else {
+      return false;
+    }
+  }
 
   @action loadProfile = async (username: string) => {
     this.loadingProfile = true;
