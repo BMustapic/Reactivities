16f815aff0fddec01b5f3723bf81012440dddd8f
bozomustapic <bozo.mustapic@dekra.com>
2020-01-10 09:46:47 +0100

    Using SignalR Groups in the client

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -26,7 +26,7 @@ export default class ActivityStore {
   @observable loading = false;
   @observable.ref hubConnection: HubConnection | null = null;
 
-  @action createHubConnection = () => {
+  @action createHubConnection = (activityId: string) => {
     this.hubConnection = new HubConnectionBuilder()
       .withUrl("http://localhost:5000/chat", {
         accessTokenFactory: () => this.rootStore.commonStore.token!
@@ -37,17 +37,30 @@ export default class ActivityStore {
     this.hubConnection
       .start()
       .then(() => console.log(this.hubConnection!.state))
+      .then(() => {
+        console.log("Attempting to join group");
+        this.hubConnection!.invoke("AddToGroup", activityId);
+      })
       .catch(err => console.log("Error establishing connection", err));
 
     this.hubConnection.on("ReceiveComment", comment => {
       runInAction("receiving comment", () => {
         this.activity!.comments.push(comment);
-      })
-    })
+      });
+    });
+
+    this.hubConnection.on("Send", message => {
+      toast.info(message);
+    });
   };
 
   @action stopHubConnection = () => {
-    this.hubConnection!.stop();
+    this.hubConnection!.invoke("RemoveFromGroup", this.activity!.id)
+      .then(() => {
+        this.hubConnection!.stop();
+      })
+      .then(() => console.log("Connection stopped"))
+      .catch(err => console.log(err));
   };
 
   @action addComment = async (values: any) => {
