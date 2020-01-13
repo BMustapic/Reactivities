c4d8a2f9d4f9a9640d2b4d660fd010ee255285a4
bozomustapic <bozo.mustapic@dekra.com>
2020-01-13 13:09:34 +0100

    FB Login
    Adding a Facebook login button to the client app

---
+++ client-app/src/features/user/SocialLogin.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props";
+import { Button, Icon } from "semantic-ui-react";
+
+interface IProps {
+  fbCallback: (response: any) => void;
+}
+
+const SocialLogin: React.FC<IProps> = ({ fbCallback }) => {
+  return <div>
+      <FacebookLogin 
+        appId="2477585689177243"
+        fields="name,email,picture"
+        callback={fbCallback}
+        render={(renderProps: any) => (
+            <Button onClick={renderProps.onClick} type="button" fluid color="facebook" >
+                <Icon name="facebook" />
+                Login with Facebook
+            </Button>
+        )}
+      />
+  </div>;
+};
+
+export default SocialLogin;
